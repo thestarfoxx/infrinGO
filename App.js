3033a4016233d0dde97b61d2166c2f1973784170
thestarfoxx <mustafaberkbacaksiz@gmail.com>
2022-11-15 23:00:12 +0300

    mapview deneme

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
+
+import {Dimensions} from 'react-native';
+import * as React from 'react';
 import { StatusBar } from 'expo-status-bar';
+import MapView from 'react-native-maps'
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView style={styles.map} />
     </View>
   );
 }
@@ -16,5 +19,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
   },
 });
