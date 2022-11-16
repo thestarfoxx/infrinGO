486726c0b62f0597683991741601c345ae077a39
thestarfoxx <mustafaberkbacaksiz@gmail.com>
2022-11-16 13:21:28 +0300

    Map Mockup
    Added a google map and mockup markers for trial

--- App.js
+++ App.js
@@ -3,12 +3,47 @@ import {Dimensions} from 'react-native';
 import * as React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import MapView from 'react-native-maps'
-import { StyleSheet, Text, View } from 'react-native';
+import { Marker } from 'react-native-maps';
+import { StyleSheet, Text, View, Button, Modal } from 'react-native';
+
+const ciragan ={
+  latitude: 41.043205458373286, 
+  longitude: 29.0098958,
+  latitudeDelta: 0.01,
+  longitudeDelta: 0.01,
+}
+
+const aylak ={
+  latitude: 41.04405557201334, 
+  longitude: 29.004565697493568,
+  latitudeDelta: 0.01,
+  longitudeDelta: 0.01,
+}
+
+const citir ={
+  latitude: 41.04803397438951, 
+  longitude: 29.002270753318555 ,
+  latitudeDelta: 0.01,
+  longitudeDelta: 0.01,
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <MapView style={styles.map} />
+      <MapView 
+        style={styles.map} 
+        initialRegion={{
+          latitude: 41.050258,
+          longitude: 29.011065,
+          latitudeDelta: 0.01,
+          longitudeDelta: 0.01,
+          }}
+     >
+      <Marker coordinate={citir} />
+      <Marker coordinate={ciragan} />
+      <Marker coordinate={aylak}/>
+     </MapView>
+     
     </View>
   );
 }
